b383afda051919c7025d36e9e8f1caa975583da1
denis <toxinden@gmail.com>
2024-11-01 17:34:40 +0500

    Json for vercel deploy

---
+++ api/index.php
@@ -0,0 +1,9 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+require_once __DIR__ . '/../src/Controller/UserController.php';
+
+//ini_set('display_errors', 0);
+//error_reporting(E_ALL & ~E_WARNING);
+
+$controller = new UserController();
+$controller->processRequest();
